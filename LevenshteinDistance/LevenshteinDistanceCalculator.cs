ebc3aa23982d99b7eabe9fd501a14b4519fd6a68
Masayuki Takeda <rexiaxm7@gmail.com>
2018-07-27 07:05:04 +0900

    Added a method with arguments as insert, replace and delete cost

--- LevenshteinDistance/LevenshteinDistanceCalculator.cs
+++ LevenshteinDistance/LevenshteinDistanceCalculator.cs
@@ -5,13 +5,15 @@ namespace LevenshteinDistance
 {
     public static class LevenshteinDistanceCalculator
     {
-        public static int Calculate(string s1, string s2)
-        {
-            if (s1 == null) throw new ArgumentNullException("s1");
-            if (s2 == null) throw new ArgumentNullException("s2");
+        public static int Calculate(string text1, string text2) => Calculate(text1, text2, 1, 1, 1);
 
-            var len1 = s1.Length;
-            var len2 = s2.Length;
+        public static int Calculate(string text1, string text2, int insertCost, int replaceCost, int deleteCost)
+        {
+            if (text1 == null) throw new ArgumentNullException("text1");
+            if (text2 == null) throw new ArgumentNullException("text2");
+
+            var len1 = text1.Length;
+            var len2 = text2.Length;
 
             if (len1 == 0) return len2;
             if (len2 == 0) return len1;
@@ -22,8 +24,13 @@ namespace LevenshteinDistance
             {
                 for (var j = 1; j < len2 + 1; j++)
                 {
-                    var distance = s1[i - 1].Equals(s2[j - 1]) ? 0 : 1;
-                    var candidates = new[] { matrix[i - 1, j] + 1, matrix[i, j - 1] + 1, matrix[i - 1, j - 1] + distance };
+                    var cost = text1[i - 1].Equals(text2[j - 1]) ? 0 : replaceCost;
+                    var candidates = new[]
+                    {
+                        matrix[i - 1, j] + insertCost,
+                        matrix[i, j - 1] + deleteCost,
+                        matrix[i - 1, j - 1] + cost
+                    };
                     matrix[i, j] = candidates.Min();
                 }
             }
