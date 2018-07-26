80d5b9d14fea62a8f2292b339848006d897d17b3
Masayuki Takeda <rexiaxm7@gmail.com>
2018-07-26 23:44:07 +0900

    Refactored LevenshteinDistance

--- LevenshteinDistance/LevenshteinDistanceCalculator.cs
+++ LevenshteinDistance/LevenshteinDistanceCalculator.cs
@@ -10,17 +10,17 @@ namespace LevenshteinDistance
             if (s1 == null) throw new ArgumentNullException("s1");
             if (s2 == null) throw new ArgumentNullException("s2");
 
-            var s1Length = s1.Length;
-            var s2Length = s2.Length;
+            var len1 = s1.Length;
+            var len2 = s2.Length;
 
-            if (s1Length == 0) return s2Length;
-            if (s2Length == 0) return s1Length;
+            if (len1 == 0) return len2;
+            if (len2 == 0) return len1;
 
-            var matrix = CreateInitializedMatrix(s1Length, s2Length);
+            var matrix = CreateInitializedMatrix(len1 + 1, len2 + 1);
 
-            for (var i = 1; i < s1Length + 1; i++)
+            for (var i = 1; i < len1 + 1; i++)
             {
-                for (var j = 1; j < s2Length + 1; j++)
+                for (var j = 1; j < len2 + 1; j++)
                 {
                     var distance = s1[i - 1].Equals(s2[j - 1]) ? 0 : 1;
                     var candidates = new[] { matrix[i - 1, j] + 1, matrix[i, j - 1] + 1, matrix[i - 1, j - 1] + distance };
@@ -28,19 +28,19 @@ namespace LevenshteinDistance
                 }
             }
 
-            return matrix[s1Length, s2Length];
+            return matrix[len1, len2];
         }
 
-        private static int[,] CreateInitializedMatrix(int s1Length, int s2Length)
+        private static int[,] CreateInitializedMatrix(int len1, int len2)
         {
-            var matrix = new int[s1Length + 1, s2Length + 1];
+            var matrix = new int[len1, len2];
 
-            for (var i = 0; i < s1Length + 1; i++)
+            for (var i = 0; i < len1; i++)
             {
                 matrix[i, 0] = i;
             }
 
-            for (var i = 0; i < s2Length + 1; i++)
+            for (var i = 0; i < len2; i++)
             {
                 matrix[0, i] = i;
             }
