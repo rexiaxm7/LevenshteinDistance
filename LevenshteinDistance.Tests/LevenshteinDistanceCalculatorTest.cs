db75d6bcdeeaf98552e85652eaaed4a475f4cf71
Masayuki Takeda <rexiaxm7@gmail.com>
2018-12-14 06:40:18 +0900

    fix Codacy issues

--- LevenshteinDistance.Tests/LevenshteinDistanceCalculatorTest.cs
+++ LevenshteinDistance.Tests/LevenshteinDistanceCalculatorTest.cs
@@ -3,17 +3,17 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace LevenshteinDistance.Tests
 {
     [TestClass]
-    public class LevenshteinDistanceCalculatorTest
+    public static class LevenshteinDistanceCalculatorTest
     {
         [TestMethod]
-        public void CalculateTest()
+        public static void CalculateTest()
         {
             var distance = LevenshteinDistanceCalculator.Calculate("string", "being");
             Assert.AreEqual(3, distance);
         }
 
         [TestMethod]
-        public void CalculateWithCostTest()
+        public static void CalculateWithCostTest()
         {
             var distance = LevenshteinDistanceCalculator.Calculate("agent", "agency", 7, 10, 7);
             Assert.AreEqual(17, distance);
